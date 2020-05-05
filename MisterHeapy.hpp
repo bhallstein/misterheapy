b9db2e3618ebd70dbe79f903bd765e923a436a16
Ben Hallstein <a@ben.am>
2020-05-05 20:44:26 +0100

    Template now to <Node> rather than <Node*>, to make pointer-ness explicit within Misterheapy. Removed requirement for MH to know anything about method of node comparison.

--- MisterHeapy.hpp
+++ MisterHeapy.hpp
@@ -20,35 +20,32 @@
 /*
  * ABOUT MISTERHEAPY
  *
- * MisterHeapy is a binary heap for storing pointers to objects.
+ * MisterHeapy is a binary heap for storing objects. You pass it pointers to your objects, which must implement
+ * operator<, and it sorts them into a heap, which can then be updated, by index or by reference.
+ *
+ * Your objects must also by memory contiguous - for instance, held in a std::vector or malloc’d array.
+ * Additionally, the first item pushed or fast-pushed to the heap must be the first in memory.
  *
  * It is bog standard, but has capabilities not provided by std's heap functions:
  *
- *     1. It supports live updating of elements by index in O(logN) time with `update_at(index, new_value)`.
- *     2. By maintaining a lookup table, MisterHeapy enables you to update any object in the heap directly,
- *        without knowing its index.
- *        For instance, to update an object *x, you can simply call `update(x, 12)`.
- *        And this is virtually as fast as updating by index.
+ *     1. It supports live updating of elements by index in (worst-case) O(logN) time with `update_at(index)`.
+ *     2. By maintaining a lookup table, MisterHeapy enables you to update any object directly, without
+ *        having to know its index in the heap.
+ *        For instance, to update an object *x, you can simply call `update(x)`.
+ *        The lookup table is O(1), so this is as fast as updating by index.
  *
  * ~~ Crafting Your Objects ~~
  *
- * DEFINITION: the "comparand" of an object is the property by which it will be ordered in the heap.
- *     i.e. if A's comparand is greater than B's comparand, A will be 'above' B in the heap.
- *     For instance, a MapLoc's comparand is a float called min_dist.
- *
- * To be used with MisterHeapy, your objects must implement:
- *     1. The function `void setComparand(new_value)`
- *           This should overwrite the object's comparand.
- *           e.g. for MapLoc: `void setComparand(float new_dist) { min_dist = new_dist; }
- *     2. The operator `<`
- *           This should take a pointer p to another object of the same class, and compare its 
- *           own comparand to p's, e.g. `bool operator< (MapLoc *p) { return min_dist < p->min_dist; }`
+ * To be used with MisterHeapy, your objects must implement operator<.
+ *    - This should take a pointer to another object X of the same class, and compare itself to X, e.g.:
+ *        bool operator< (const MyNode *p) {
+ *            return this->x < p->x;
+ *        }
  * 
  * ~~ Creating Your MisterHeapy Instance ~~
  * 
- * MisterHeapy is a templated class, adapting to the type of your object, and of your object's comparand.
- * So, if your object type is `Node`, and a Node's comparand `dist` is a float:
- *     `MisterHeapy<Node*, float> myheapy(32);`
+ * MisterHeapy is a templated class. If your object type is `Node`:
+ *     `MisterHeapy<Node> myheapy(32);`
  * The parameter is the maximum size of your heap. Attempting to push elements beyond this limit will fail.
  *
  * ~~ Initializing Your Heap ~~
@@ -75,7 +72,7 @@
 
 inline int log_base2(unsigned int x) {
 	int ind = 0;
-	while (x >>= 1) ind++;
+	while (x >>= 1) ++ind;
 	return ind;
 }
 inline int twotothepowerof(unsigned int x) {
@@ -83,7 +80,7 @@ inline int twotothepowerof(unsigned int x) {
 }
 
 
-template <class nodetype, typename comparandtype>
+template <class nodetype>
 class MisterHeapy {
 public:
 	MisterHeapy(int max_size);
@@ -98,15 +95,15 @@ public:
 								// Where possible, reuse heaps rather than creating new ones – this is faster, since
 								// it misses out the memory allocations that occur on construct.
 
-	void fast_push(nodetype x);	// Push onto the heap without sorting into place. Use to initialize, then call...
-	void reheapify();			// Set the heap back in order.
+	void fast_push(nodetype *x);	// Push onto the heap without sorting into place. Use to initialize, then call...
+	void reheapify();				// Set the heap back in order.
 
-	void push(nodetype);
-	nodetype pop();
+	void push(nodetype*);
+	nodetype* pop();
 	int size();
 
-	void update(nodetype x, comparandtype new_val);		// Update a stored object to a new comparand value
-	void update_at(int i, comparandtype new_val);		// Update at a known location in the underlying array
+	void update(nodetype *x);	// Move the given node up/down the heap until order is restored
+	void update_at(int i);		// Do the same for the node at the given location
 
 private:
 	// Methods
@@ -118,23 +115,23 @@ private:
 	// Properties
 	int n;						// Allocated size
 	int length;					// Current size
-	nodetype *heap;				// The heap
+	nodetype **heap;				// The heap
 	int *indices_in_heap;		// indices_in_heap[pointer to x] = index in heap of x
-	nodetype x_start;		  	// Pointer to first X
+	nodetype *x_start;		  	// Pointer to first X
 };
 
 
 /* Implementation (a templated class's implementation must be in the same file as its declaration.) */
 
-template <class nodetype, typename comparandtype>
-MisterHeapy<nodetype, comparandtype>::MisterHeapy(int _n) : n(_n) {
-	heap = (nodetype*) malloc(sizeof(nodetype) * n);
+template <class nodetype>
+MisterHeapy<nodetype>::MisterHeapy(int _n) : n(_n) {
+	heap = (nodetype**) malloc(sizeof(nodetype*) * n);
 	indices_in_heap = (int*) malloc(sizeof(int) * n);
 	reset();
 }
 
-template <class nodetype, typename comparandtype>
-MisterHeapy<nodetype, comparandtype>::MisterHeapy(const MisterHeapy &h) :
+template <class nodetype>
+MisterHeapy<nodetype>::MisterHeapy(const MisterHeapy &h) :
 	n(0),
 	length(0),
 	x_start(0),
@@ -144,37 +141,37 @@ MisterHeapy<nodetype, comparandtype>::MisterHeapy(const MisterHeapy &h) :
 	copyFrom(h);
 }
 
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::copyFrom(const MisterHeapy &h) {
+template <class nodetype>
+void MisterHeapy<nodetype>::copyFrom(const MisterHeapy &h) {
 	if (n != h.n) {
 		if (heap) {
 			free(heap);
 			free(indices_in_heap);
 		}
-		heap = (nodetype*) malloc(sizeof(nodetype) * h.n);
+		heap = (nodetype**) malloc(sizeof(nodetype*) * h.n);
 		indices_in_heap = (int*) malloc(sizeof(int) * h.n);
 	}
 	n = h.n;
 	length = h.length;
 	x_start = h.x_start;
 	memcpy(indices_in_heap, h.indices_in_heap, sizeof(int) * n);
-	memcpy(heap, h.heap, sizeof(nodetype) * n);
+	memcpy(heap, h.heap, sizeof(nodetype*) * n);
 }
 
 
-template <class nodetype, typename comparandtype>
-MisterHeapy<nodetype, comparandtype>::~MisterHeapy() {
+template <class nodetype>
+MisterHeapy<nodetype>::~MisterHeapy() {
 	free(heap);
 	free(indices_in_heap);
 }
 
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::reset() {
+template <class nodetype>
+void MisterHeapy<nodetype>::reset() {
 	length = 0;
 }
 
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::fast_push(nodetype x) {
+template <class nodetype>
+void MisterHeapy<nodetype>::fast_push(nodetype *x) {
 	if (length >= n)
 		return;
 	int i = length++;
@@ -183,9 +180,9 @@ void MisterHeapy<nodetype, comparandtype>::fast_push(nodetype x) {
 	indices_in_heap[x - x_start] = i;
 }
 
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::swap_nodes(int ind1, int ind2) {
-	nodetype x1 = heap[ind1], x2 = heap[ind2];
+template <class nodetype>
+void MisterHeapy<nodetype>::swap_nodes(int ind1, int ind2) {
+	nodetype *x1 = heap[ind1], *x2 = heap[ind2];
 	// Update at ind1
 	heap[ind1] = x2;
 	indices_in_heap[x2 - x_start] = ind1;
@@ -193,8 +190,8 @@ void MisterHeapy<nodetype, comparandtype>::swap_nodes(int ind1, int ind2) {
 	heap[ind2] = x1;
 	indices_in_heap[x1 - x_start] = ind2;
 }
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::up_heap(int _ind) {
+template <class nodetype>
+void MisterHeapy<nodetype>::up_heap(int _ind) {
 	int ind = _ind, parent_ind = (ind-1)/2;
 	
 	while (ind > 0 && *heap[parent_ind] < heap[ind]) {
@@ -203,10 +200,10 @@ void MisterHeapy<nodetype, comparandtype>::up_heap(int _ind) {
 		parent_ind = (ind-1)/2;
 	}
 }
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::down_heap(int _ind) {
+template <class nodetype>
+void MisterHeapy<nodetype>::down_heap(int _ind) {
 	int ind = _ind, l_child_ind = ind*2+1, r_child_ind = ind*2+2, larger_child_ind;
-	nodetype our_node = heap[ind];
+	nodetype *our_node = heap[ind];
 	
 	while (l_child_ind < length) {
 		if (r_child_ind < length)
@@ -223,15 +220,15 @@ void MisterHeapy<nodetype, comparandtype>::down_heap(int _ind) {
 	}
 }
 
-template <class nodetype, typename comparandtype>
-nodetype MisterHeapy<nodetype, comparandtype>::pop() {
-	nodetype popsicle = heap[0];
+template <class nodetype>
+nodetype* MisterHeapy<nodetype>::pop() {
+	nodetype *node_to_pop = heap[0];
 	swap_nodes(0, --length);
 	down_heap(0);
-	return popsicle;
+	return node_to_pop;
 }
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::push(nodetype x) {
+template <class nodetype>
+void MisterHeapy<nodetype>::push(nodetype *x) {
 	if (length >= n)
 		return;
 	if (length == 0) x_start = x;
@@ -251,8 +248,8 @@ void MisterHeapy<nodetype, comparandtype>::push(nodetype x) {
  *                                                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::reheapify() {
+template <class nodetype>
+void MisterHeapy<nodetype>::reheapify() {
 	// To sort a heap: start at the lowest level, calling down_heap on the root of each subtree,
 	// and do this for each level until the top of the tree is reached
 	int i, j, depth, greatest_depth = log_base2(length);
@@ -262,23 +259,21 @@ void MisterHeapy<nodetype, comparandtype>::reheapify() {
 			if (i < length)
 				down_heap(i);
 }
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::update_at(int i, comparandtype new_val) {
+template <class nodetype>
+void MisterHeapy<nodetype>::update_at(int i) {
 	if (i < 0 || i >= length) return;
-	heap[i]->setComparand(new_val);
 	int parent_i = (i-1)/2;	
 	if (i == 0 || *heap[i] < heap[parent_i])
 		down_heap(i);
 	else
 		up_heap(i);
 }
-template <class nodetype, typename comparandtype>
-void MisterHeapy<nodetype, comparandtype>::update(nodetype x, comparandtype new_val) {
-	int i = indices_in_heap[x - x_start];
-	update_at(i, new_val);
+template <class nodetype>
+void MisterHeapy<nodetype>::update(nodetype *x) {
+	update_at(indices_in_heap[x - x_start]);
 }
-template <class nodetype, typename comparandtype>
-int MisterHeapy<nodetype, comparandtype>::size() {
+template <class nodetype>
+int MisterHeapy<nodetype>::size() {
 	return length;
 }
 
--- main.cpp
+++ main.cpp
@@ -10,28 +10,28 @@
 class Node {
 public:
 	Node() { }
-	bool operator< (Node *n) {
-		return comparand < n->comparand;
+	bool operator< (const Node *n) {
+		return x < n->x;
 	}
-	float comparand;
+	float x;
 };
 
 int main() {
 	int n = 10;
 	
-	MisterHeapy<Node*, float> heapy(n);		// Create our MisterHeapy instance
-	std::vector<Node> nodes;				// Allocate nodes
+	MisterHeapy<Node> heapy(n);		// Create our MisterHeapy instance
+	std::vector<Node> nodes;			// Allocate nodes
 	nodes.resize(n);
 	
 	for (int i=0; i < n; i++) {
-		nodes[i].comparand = i;				// Populate heapy
+		nodes[i].x = i;				// Populate heapy
 		heapy.fast_push(&nodes[i]);			// Since we're using fast_push, will be disordered
 	}
 
-	std::cout << "Last in heap: " << heapy.pop()->comparand << ", reheapifying..." << std::endl;
+	std::cout << "Popping heap: " << heapy.pop()->x << ", reheapifying..." << std::endl;
 	heapy.reheapify();
-	std::cout << "Last in heap: " << heapy.pop()->comparand << std::endl;
-	std::cout << "Last in heap: " << heapy.pop()->comparand << std::endl;
+	std::cout << "Popping heap: " << heapy.pop()->x << std::endl;
+	std::cout << "Popping heap: " << heapy.pop()->x << std::endl;
 
 	return 0;
 }
