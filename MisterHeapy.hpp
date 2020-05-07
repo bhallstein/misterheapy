278012e6d92592fa003850d972d441332fbb9cc7
Ben Hallstein <a@ben.am>
2020-05-07 12:10:16 +0100

    Minor changes

--- MisterHeapy.hpp
+++ MisterHeapy.hpp
@@ -1,8 +1,8 @@
 /*
  * MisterHeapy.hpp
- * 
+ *
  * A heap.
- * 
+ *
  * Copyright (C) 2012 - Ben Hallstein - http://ben.am - :-)
  * Published under the MIT license: http://opensource.org/licenses/MIT
  *
@@ -10,13 +10,13 @@
 
 /*
  * ABOUT MISTERHEAPY
- * 
+ *
  * MisterHeapy is a binary heap for storing objects. You pass it pointers to your objects, which must implement
  * operator<, and it sorts them into a heap, which can then be updated, by index or by reference.
- * 
+ *
  * Your objects must also by memory contiguous - for instance, held in std::vector or an array.
  * Additionally, the first item pushed or fast-pushed to the heap must be the first in memory.
- * 
+ *
  * It is bog standard, but has capabilities not provided by std's heap functions:
  *
  *     1. It supports live updating of elements by index in average O(1) time (worst-case logN) with `update_at(index)`.
@@ -24,34 +24,34 @@
  *        its index in the heap.
  *        For instance, to update an object *x, you can simply call `update(x)`.
  *        The lookup table is O(1), so this is as fast as updating by index.
- * 
- * 
+ *
+ *
  * ~~ Crafting your objects ~~
- * 
+ *
  * To be used with MisterHeapy, your objects must implement operator<.
  *    - This should take a pointer to another object X of the same class, and compare itself to X, e.g.:
  *        bool operator< (const MyNode *p) {
  *            return this->x < p->x;
  *        }
- * 
- * 
+ *
+ *
  * ~~ Creating a MisterHeapy instance ~~
- * 
+ *
  * MisterHeapy is a templated class. If your object type is `Node`:
  *     `MisterHeapy<Node> myheapy(32);`
  * The parameter is the maximum size of your heap. Attempting to push elements beyond this limit will fail.
- * 
- * 
+ *
+ *
  * ~~ Initializing the heap ~~
- * 
+ *
  * To set up your initial heap, you can call `push` to add elements.
  *   - If you need to feed it a large initial dataset, you may use fast_push instead, which prevents objects from
  *     being sorted as they are added.
  *   - Since this disorders the heap, though, you MUST then call `reheapify`, which is O(n).
- * 
- * 
+ *
+ *
  * ~~ IMPORTANT WARNING ~~
- * 
+ *
  * MisterHeapy assumes some things about your elements:
  *   - that they are all contiguous in memory (i.e. they are held in a vector or contiguously allocated)
  *   - that the first element pushed or fast_pushed after instantiating or resetting() IS THE FIRST in said
@@ -60,13 +60,11 @@
  *
  * Note: you can disable index tracking, and the ability to update by reference, by #defining _MH_NO_INDEX_TRACKING
  *       before #including MisterHeapy.h
- * 
+ *
  */
 
 #ifndef MISTERHEAPY_H
 #define MISTERHEAPY_H
-
-#include <stdlib.h>
 
 #define MH_2_TO_THE(x) (1 << (x))
 
@@ -81,14 +79,14 @@ class MisterHeapy {
 public:
 	MisterHeapy(int max_size);
 	~MisterHeapy();
-	
+
 	// Copy constr.
 	MisterHeapy(const MisterHeapy &);
 	void copyFrom(const MisterHeapy &);
-	
-	
+
+
 	void reset();				// Makes the heap effectively as-new.
-								// Where possible, reuse heaps rather than creating new ones – this skips the 
+								// Where possible, reuse heaps rather than creating new ones – this skips the
 								// memory allocations that occur on construct.
 
 	void fast_push(nodetype *x);	// Push onto the heap without sorting into place. Use to initialize, then call...
@@ -109,18 +107,18 @@ private:
 	void up_heap(int _ind);
 	void down_heap(int _ind);
 	void sort_heap();
-	
+
 	static inline int log_base2(unsigned int x) {
 		int ind = 0;
 		while (x >>= 1) ++ind;
 		return ind;
 	}
-	
+
 	// Properties
 	int n;						// Allocated size
 	int length;					// Current size
 	nodetype **heap;			// The heap
-	
+
 _MH_IT(
 	int *indices_in_heap;		// indices_in_heap[pointer to x] = index in heap of x
 	nodetype *x_start;			// Pointer to first X
@@ -210,7 +208,7 @@ inline void MisterHeapy<nodetype>::swap_nodes(int ind1, int ind2) {
 template <class nodetype>
 void MisterHeapy<nodetype>::up_heap(int _ind) {
 	int ind = _ind, parent_ind = (ind-1)/2;
-	
+
 	while (ind > 0 && *heap[parent_ind] < heap[ind]) {
 		swap_nodes(parent_ind, ind);
 		ind = parent_ind;
@@ -221,7 +219,7 @@ template <class nodetype>
 void MisterHeapy<nodetype>::down_heap(int _ind) {
 	int ind = _ind, l_child_ind = ind*2+1, r_child_ind = ind*2+2, larger_child_ind;
 	nodetype *our_node = heap[ind];
-	
+
 	while (l_child_ind < length) {
 		if (r_child_ind < length)
 			larger_child_ind = (*heap[l_child_ind] < heap[r_child_ind] ? r_child_ind : l_child_ind);
@@ -251,7 +249,7 @@ void MisterHeapy<nodetype>::push(nodetype *x) {
 	heap[length] = x;
 	_MH_IT(
 		if (length == 0) x_start = x;
-		indices_in_heap[x - x_start] = length++;	
+		indices_in_heap[x - x_start] = length++;
 	)
 	up_heap(length - 1);
 }
@@ -280,7 +278,7 @@ void MisterHeapy<nodetype>::reheapify() {
 template <class nodetype>
 void MisterHeapy<nodetype>::update_at(int i) {
 	if (i >= length) return;
-	int parent_i = (i-1)/2;	
+	int parent_i = (i-1)/2;
 	if (i == 0 || *heap[i] < heap[parent_i])
 		down_heap(i);
 	else
@@ -298,3 +296,4 @@ int MisterHeapy<nodetype>::size() {
 }
 
 #endif
+
--- Readme.md
+++ Readme.md
@@ -1,17 +1,16 @@
 # About MisterHeapy
 
-MisterHeapy is a templated C++ binary heap class.
+A C++ binary heap.
  
-Objects stored in a MisterHeapy can be “updated” (i.e. resorted after update) by index or by reference, both in O(1) time on average.
+Entries can be “updated” (resorted after update) by index **or by reference** both in constant time on average.
 
 For simplicity and performance, MisterHeapy places the following restrictions on objects:
 
-- They must all be memory contiguous.
+- They must be memory contiguous.
 - The first item pushed to the heap must be the first item in memory.
 
 For further details e.g. fast initialisation, see the notes at the top of MisterHeapy.hpp.
 
-MisterHeapy is published under the MIT license.
+-- Published under the MIT license
+-- BH May 2012
 
-Ben Hallstein, May 2012
-http://ben.am
